ec947e3a9e64271db90cb76dd8bb4d0eb2e0ddfe
Roman Scherer <roman.scherer@burningswell.com>
2010-02-04 01:22:50 +0100

    Added maven-gae-plugin repositiory for Google App Engine v1.3.0 JARs and removed the installation shell script.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0"]
-                     [com.google/appengine-api-1.0-sdk "1.3.0"]
-                     [com.google/appengine-api-labs "1.3.0"]
-                     [com.google/appengine-api-stubs "1.3.0"]
-                     [com.google/appengine-local-runtime "1.3.0"]])
+                     [com.google.appengine/appengine-api-1.0-sdk "1.3.0"]
+                     [com.google.appengine/appengine-api-labs "1.3.0"]
+                     [com.google.appengine/appengine-api-stubs "1.3.0"]
+                     [com.google.appengine/appengine-local-runtime "1.3.0"]]
+  :repositories [["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
