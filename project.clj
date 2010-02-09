0278ac2e7eec28c4a3a0bc67c3d68ea30ab0b7c2
Roman Scherer <roman.scherer@burningswell.com>
2010-02-09 15:08:42 +0100

    Added lein-clojars to development dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject appengine-clj "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [com.google.appengine/appengine-api-1.0-sdk "1.3.0"]
                      [com.google.appengine/appengine-api-labs "1.3.0"]
                      [com.google.appengine/appengine-api-stubs "1.3.0"]
-                     [com.google.appengine/appengine-local-runtime "1.3.0"]]
+                     [com.google.appengine/appengine-local-runtime "1.3.0"]
+                     [swank-clojure "1.1.0"]]
   :repositories [["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
