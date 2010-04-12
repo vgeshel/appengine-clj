452c08c5160486d1dbb0f216be3293eedc3121bf
Roman Scherer <roman@soundclaat.(none)>
2010-04-13 05:44:51 +0800

    Added autodoc.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject appengine "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.1.0"]
 		 [inflections "0.2"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+  :dev-dependencies [[autodoc "0.7.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
 		     [leiningen/lein-swank "1.1.0"]
 		     [com.google.appengine/appengine-api-1.0-sdk "1.3.2"]
 		     [com.google.appengine/appengine-api-labs "1.3.2"]
