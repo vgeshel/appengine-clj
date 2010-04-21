51e59f9f67281902da87fdcb97ee56ce5777d838
Roman Scherer <roman@soundcloud.com>
2010-04-21 20:10:57 +0200

    Updated project file.

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject appengine "0.0.1-SNAPSHOT"
+(defproject appengine "0.1"
+  :author "John D. Hume, Roman Scherer, Jean-Denis Greze"
+  :description "Clojure library for Google App Engine."
+  :url "http://github.com/r0man/appengine-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
-		 [inflections "0.2"]]
+		 [inflections "0.3"]]
   :dev-dependencies [[autodoc "0.7.0"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]
+                     [lein-clojars "0.5.0"]
 		     [leiningen/lein-swank "1.1.0"]
 		     [com.google.appengine/appengine-api-1.0-sdk "1.3.2"]
 		     [com.google.appengine/appengine-api-labs "1.3.2"]
 		     [com.google.appengine/appengine-api-stubs "1.3.2"]
 		     [com.google.appengine/appengine-local-runtime "1.3.2"]
 		     [com.google.appengine/appengine-testing "1.3.2"]]
-  :repositories [["maven-gae-plugin" 
-		  "http://maven-gae-plugin.googlecode.com/svn/repository"]])
+  :repositories [["clojars" "http://clojars.org/repo"]
+                 ["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
