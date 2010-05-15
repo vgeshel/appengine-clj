43ed345c22756b77456fdad15d3cf4617e17252a
Roman Scherer <roman@soundcloud.com>
2010-05-15 22:02:21 +0200

    Added #map-keys and #map-keyword fns.

--- src/appengine/utils.clj
+++ src/appengine/utils.clj
@@ -4,3 +4,13 @@
 
 (defn compact [seq]
   (remove nil? seq))
+
+(defn map-keys
+  "Returns a lazy sequence consisting of the result of applying f to
+  the keys of coll."
+  [coll f] (zipmap (map f (keys coll)) (vals coll)))
+
+(defn map-keyword [coll]
+  "Returns a lazy sequence consisting of the result of applying
+  #'keyword to the keys of coll."
+  (map-keys coll #'keyword))
--- test/appengine/test/utils.clj
+++ test/appengine/test/utils.clj
@@ -5,3 +5,13 @@
   (is (= (compact []) []))
   (is (= (compact [1 nil 2]) [1 2]))
   (is (= (compact [1 2]) [1 2])))
+
+(deftest test-map-keys-with-keyword
+  (is (= (map-keys {"a" 1 "b" 2} #'identity) {"a" 1 "b" 2}))
+  (is (= (map-keys {"a" 1 "b" 2} #'keyword) {:a 1 :b 2})))
+
+(deftest test-map-keyword
+  (is (= (map-keyword {"a" 1 "b" 2}) {:a 1 :b 2}))
+  (is (= (map-keyword {'a 1 'b 2}) {:a 1 :b 2}))
+  (is (= (map-keyword {:a 1 :b 2}) {:a 1 :b 2})))
+
