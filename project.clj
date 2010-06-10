4fbed6a59837b98d6582f7b234a6bdb66c04fbab
Roman Scherer <roman@soundcloud.com>
2010-06-10 16:55:57 +0200

    Added clojure 1.2 compatible ring dependency.

--- project.clj
+++ project.clj
@@ -4,16 +4,17 @@
   :url "http://github.com/r0man/appengine-clj"
   :autodoc {:name "App Engine"
             :copyright "Copyright (c) 2009, 2010 John D. Hume, Roman Scherer, Jean-Denis Greze, E.Fukamachi"}
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+  :dependencies [[inflections "0.4-SNAPSHOT"]
+                 [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [inflections "0.4-SNAPSHOT"]]
-  :dev-dependencies [;[autodoc "0.7.1"]
-                     [lein-clojars "0.5.0"]
+                 [org.clojars.paraseba/ring-servlet "0.2.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]
                      [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
                      [com.google.appengine/appengine-api-labs "1.3.4"]
                      [com.google.appengine/appengine-api-stubs "1.3.4"]
                      [com.google.appengine/appengine-local-runtime "1.3.4"]
+                     ;; [com.google.appengine/appengine-local-runtime-shared "1.3.4"]
                      [com.google.appengine/appengine-testing "1.3.4"]
+                     [org.clojars.paraseba/ring-jetty-adapter "0.2.0"]
                      [swank-clojure "1.2.1"]]
-  :repositories [["clojars" "http://clojars.org/repo"]
-                 ["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
+  :repositories [["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
