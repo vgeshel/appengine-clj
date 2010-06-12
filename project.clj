a1006c253f8062406e8fbb7db72ee5bf5a6c7251
Roman Scherer <roman@soundcloud.com>
2010-06-12 14:11:28 +0200

    Added app engine sdk to dependencies.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :url "http://github.com/r0man/appengine-clj"
   :autodoc {:name "App Engine"
             :copyright "Copyright (c) 2009, 2010 John D. Hume, Roman Scherer, Jean-Denis Greze, E.Fukamachi"}
-  :dependencies [[inflections "0.4-SNAPSHOT"]
+  :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
+                 [inflections "0.4-SNAPSHOT"]
                  [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.clojars.paraseba/ring-servlet "0.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
-                     [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
                      [com.google.appengine/appengine-api-labs "1.3.4"]
                      [com.google.appengine/appengine-api-stubs "1.3.4"]
                      [com.google.appengine/appengine-local-runtime "1.3.4"]
