1817de37036fc169998969bc21f2ab0e0a6b2f15
Roman Scherer <roman@soundcloud.com>
2010-06-20 01:28:26 +0200

    Extended ISeq with record protocol.

--- src/appengine/datastore/protocols.clj
+++ src/appengine/datastore/protocols.clj
@@ -3,19 +3,27 @@
   appengine.datastore.protocols)
 
 (defprotocol Record
-  (create [entities]
-    "Create new entities in the datastore. If one of the entities
-    already exists, the functions throws an exception.")
-  (delete [entities]
-    "Delete the keys or entities from the datastore.")
-  (lookup [entities]
-    "Lookup entities from the datastore.")
-  (save [entities]
-    "Save the entities in the datastore.")
-  (update [entities key-vals]
-    "Update the entities with the key-vals and save them in the
+  (create [record]
+    "Create new record in the datastore. If the record already exists,
+    the functions throws an exception.")
+  (delete [record]
+    "Delete the record from the datastore.")
+  (lookup [record]
+    "Lookup the record from the datastore.")
+  (save [record]
+    "Save the record in the datastore.")
+  (update [record key-vals]
+    "Update the record with the key-vals and save it in the
     datastore."))
 
 (defprotocol Lifecycle
   (deserialize [object] "Deserialize an object from the datastore.")
-  (serialize [record] "Serialize the datastore record into an entity."))
+  (serialize [record] "Serialize the record into an entity."))
+
+(extend-type clojure.lang.ISeq
+  Record
+  (create [records] (map create records))
+  (delete [records] (map delete records))
+  (lookup [records] (map lookup records))
+  (save   [records] (map save records))
+  (update [records key-vals] (map #(update % key-vals) records)))
---
+++ test/appengine/datastore/test/protocols.clj
@@ -0,0 +1,31 @@
+(ns appengine.datastore.test.protocols
+  (:import (com.google.appengine.api.datastore Entity))
+  (:use appengine.datastore.entities
+        appengine.datastore.keys
+        appengine.datastore.protocols
+        appengine.test
+        clojure.test))
+
+(defn make-sequence []
+  (seq [(make-key "continent" "eu")
+        (Entity. (make-key "continent" "af"))]))
+
+(datastore-test test-create
+  (let [entities (create (make-sequence))]
+    (is (seq? entities))))
+
+(datastore-test test-delete
+  (let [entities (create (make-sequence))]
+    (is (delete entities))))
+
+(datastore-test test-lookup
+  (let [entities (create (make-sequence))]
+    (is (lookup entities))))
+
+(datastore-test test-save
+  (let [entities (create (make-sequence))]
+    (is (save entities))))
+
+(datastore-test test-update
+  (let [entities (create (make-sequence))]
+    (is (update entities {:updated-at "TIMESTAMP"}))))
