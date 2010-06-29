2187ae17c2ed825190baa1e3c94d4d8b56f99411
Roman Scherer <roman@burningswell.com>
2010-06-29 21:16:33 +0200

    Added directory option to start-server.

--- src/appengine/server.clj
+++ src/appengine/server.clj
@@ -4,14 +4,35 @@
         [ring.adapter.jetty :only (run-jetty)]))
 
 (defn start-server
-  "Start the server."
+  "Start the server.
+
+Example:
+
+  (start-server \"Hello World\" :directory \"/tmp\" :join? false :port 8080)
+  ; => #<Server Server@1a2cc7>
+"
   [application & options]
-  (dosync
-   (init-appengine)     
-   (apply run-jetty application options)))
+  (let [options (apply hash-map options)
+        directory (or (:directory options) "war")]
+    (dosync
+     (init-appengine directory)
+     (run-jetty application options))))
 
 (defn stop-server
-  "Stop the server."
+  "Stop the server.
+
+Example:
+
+  (def *server* (start-server \"Hello World\" :directory \"/tmp\" :join? false :port 8080))
+  ; => #<Server Server@1a2cc7>
+
+  (stop-server *server*)
+  ; => #<Server Server@1a2cc7>
+"
   [server]
   (do (.stop server)
       server))
+
+;; (def *server* (start-server "Hello World" :directory "/tmp" :join? false :port 8123))
+;; *server*
+;; (stop-server *server*)
--- test/appengine/test/server.clj
+++ test/appengine/test/server.clj
@@ -3,15 +3,15 @@
   (:use clojure.test appengine.server))
 
 (def *handler* "Hello World")
-(def *options* {:join? false :port 8123})
+(def *options* [:join? false :port 8123])
 
 (deftest test-start-server
-  (let [server (start-server *handler* *options*)]
+  (let [server (apply start-server *handler* *options*)]
     (is (isa? (class server) Server))
     (is (.isStarted server))
     (stop-server server)))
 
 (deftest test-stop-server
-  (let [server (start-server *handler* *options*)]
+  (let [server (apply start-server *handler* *options*)]
     (is (isa? (class (stop-server server)) Server))
     (is (not (.isStarted server)))))
