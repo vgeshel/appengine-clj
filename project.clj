c47d6763f3e67d2633054229cfcd54c3b9b7e794
Roman Scherer <roman@burningswell.com>
2010-07-02 16:23:59 +0200

    Upgrade to 1.3.5 sdk.

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :url "http://github.com/r0man/appengine-clj"
   :autodoc {:name "Google App Engine for Clojure"
             :copyright "Copyright (c) 2009, 2010 John D. Hume, Roman Scherer, Jean-Denis Greze, E.Fukamachi"}
-  :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
+  :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.3.5"]
                  [inflections "0.4-SNAPSHOT"]
                  [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [ring/ring-servlet "0.2.3"]]
   :dev-dependencies [[autodoc "0.7.1-SNAPSHOT"]
                      [lein-clojars "0.5.0"]
-                     [com.google.appengine/appengine-api-labs "1.3.4"]
-                     [com.google.appengine/appengine-api-stubs "1.3.4"]
-                     [com.google.appengine/appengine-local-runtime "1.3.4"]
-                     ;; [com.google.appengine/appengine-local-runtime-shared "1.3.4"]
-                     [com.google.appengine/appengine-testing "1.3.4"]
+                     [com.google.appengine/appengine-api-labs "1.3.5"]
+                     [com.google.appengine/appengine-api-stubs "1.3.5"]
+                     [com.google.appengine/appengine-local-runtime "1.3.5"]
+                     [com.google.appengine/appengine-local-runtime-shared "1.3.5"]
+                     [com.google.appengine/appengine-testing "1.3.5"]
                      [ring/ring-jetty-adapter "0.2.3"]
                      [swank-clojure "1.2.1"]]
   :repositories [["maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"]])
