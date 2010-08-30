850bfbb53c86e9dea0dab32aaecce14d39f7e3d1
Roman Scherer <roman@burningswell.com>
2010-08-30 21:33:37 +0200

    Applied bug fix from chapmanb.

--- src/appengine/users.clj
+++ src/appengine/users.clj
@@ -37,7 +37,7 @@
   "With no arguments, returns a UserService and User for the current request in a map keyed by :user-service and :user respectively.
   If the user is not logged in, :user will be nil.
   With a single map argument, a Ring request, returns the user-info map associated with the request by wrap-with-user-info."
-  ([] {:user (.getCurrentUser user-service) :user-service (user-service)})
+  ([] {:user (.getCurrentUser (user-service)) :user-service (user-service)})
   ([request]
    (:appengine/user-info request)))
 
